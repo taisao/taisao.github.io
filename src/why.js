ce29ab78d17bad1573b73c67291b42cdab9621f0
taisao <taisao@users.noreply.github.com>
2015-05-12 00:45:53 +0700

    why

---
+++ src/why.js
@@ -0,0 +1,14 @@
+var Why = React.createClass({
+	render: function () {
+		return (
+			<div className="why">
+				Hello World
+			</div>
+		);
+	}
+});
+
+React.render(
+	<Why />,
+	document.body
+);
